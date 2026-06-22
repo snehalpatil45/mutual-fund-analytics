a701373802a26222ed7f287fd0eff6bde1017a30
snehal patil <patilsnehal9307484553@gmail.com>
2026-06-22 16:15:46 +0530

    Updated data ingestion script

--- scripts/data_ingestion.py
+++ scripts/data_ingestion.py
@@ -1,4 +1,18 @@
+# data_ingestion.py
+
 import pandas as pd
 import os
 
-print("Data Ingestion Started")
+folder = "data/raw"
+
+for file in os.listdir(folder):
+    if file.endswith(".csv"):
+        df = pd.read_csv(os.path.join(folder, file))
+
+        print("\n", "="*50)
+        print(file)
+        print("Shape:", df.shape)
+        print("Data Types:")
+        print(df.dtypes)
+        print("\nFirst 5 Rows:")
+        print(df.head())
